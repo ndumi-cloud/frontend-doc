3cf2eb6e15d460cbb180c5360a034fac7e1b6e04
ndumi-cloud <ndumigrace71@gmail.com>
2024-07-13 14:59:15 +0300

    last commit

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://backend-doc-1-t3zg.onrender.com/'
+export const BASE_URL = 'https://backend-doc-1-t3zg.onrender.com/api/v1'
 export const token = localStorage.getItem('token')
